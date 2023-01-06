ced82fac25cf3bcaf3d08bbd25012ea9071d6587
snowlizard <illegal_burrito@live.com>
2023-01-06 14:07:36 -0800

    added login out function

--- src/components/side-panel.jsx
+++ src/components/side-panel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import myApp from '../js/firebase';
 import store from '../js/store';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import {
   Appbar,
   Button,
@@ -17,70 +17,79 @@ import {
 const auth = getAuth(myApp);
 const provider = new GoogleAuthProvider();
 
-const SidePanel = () => (
-<Panel left>
-    <Appbar>
-        <div className='left'>
-            <Button
-                small
-                raised
-                fill color='white'
-                style={{color:'red'}}>
-                Add File
-            </Button>
-            <Button
-                small
-                raised
-                fill color='white'
-                style={{color:'red', 'marginLeft': '4px'}}
-                onClick={signIn}>
-                Login
-            </Button>
-        </div>
+const SidePanel = () => {
+    const loggedIn = useStore('login');
 
-        <div className='right'>
-            <Link
-            searchbarEnable=".searchbar-bar"
-            iconIos="f7:search"
-            iconAurora="f7:search"
-            iconMd="material:search"
-            ></Link>
-        </div>
-        <Searchbar
-            className="searchbar-bar"
-            expandable
-            searchContainer=".notes-list"
-            searchIn=".item-title"
-            disableButton={!theme.aurora}
-        ></Searchbar>
-    </Appbar>
+    const signIn = () => {
+        if (loggedIn) {
+            signOut(auth).then( () => {
+                store.dispatch('setLogin');
+            });
+        } else {
+            signInWithPopup(auth, provider)
+            .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            store.dispatch('setLogin');
+            }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            })
+        }
+    };
 
-    <List className="searchbar-not-found">
-        <ListItem title="Nothing found"></ListItem>
-    </List>
+    return (
+        <Panel left>
+            <Appbar>
+                <div className='left'>
+                    <Button
+                        small
+                        raised
+                        fill color='white'
+                        style={{color:'red'}}>
+                        Add File
+                    </Button>
+                    <Button
+                        small
+                        raised
+                        fill color='white'
+                        style={{color:'red', 'marginLeft': '4px'}}
+                        onClick={signIn}>
+                        { loggedIn ? 'Logout' : 'Login' }
+                    </Button>
+                </div>
 
-    <List className="notes-list searchbar-found">
-        <ListItem title="Acura"></ListItem>
-        <ListItem title="Audi"></ListItem>
-        <ListItem title="BMW"></ListItem>
-    </List>
-</Panel>
-);
+                <div className='right'>
+                    <Link
+                    searchbarEnable=".searchbar-bar"
+                    iconIos="f7:search"
+                    iconAurora="f7:search"
+                    iconMd="material:search"
+                    ></Link>
+                </div>
+                <Searchbar
+                    className="searchbar-bar"
+                    expandable
+                    searchContainer=".notes-list"
+                    searchIn=".item-title"
+                    disableButton={!theme.aurora}
+                ></Searchbar>
+            </Appbar>
 
-const signIn = () => {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-    });
+            <List className="searchbar-not-found">
+                <ListItem title="Nothing found"></ListItem>
+            </List>
 
-    store.dispatch('setLogin');
-  }
+            <List className="notes-list searchbar-found">
+                <ListItem title="Acura"></ListItem>
+                <ListItem title="Audi"></ListItem>
+                <ListItem title="BMW"></ListItem>
+            </List>
+        </Panel>
+    );
+};
 
 export default SidePanel;
