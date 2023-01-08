f1312ad31a47ca5b8c7e22af897c108afb65fe5b
snowlizard <illegal_burrito@live.com>
2023-01-08 01:20:16 -0800

    added firebase db notes

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import store from '../js/store';
+import { useStore } from "framework7-react";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,6 +13,23 @@ const firebaseConfig = {
     appId: "1:362249215551:web:02a5e7390280562f20628a"
 };
 
-const myApp = initializeApp(firebaseConfig);
+const myApp    = initializeApp(firebaseConfig);
+const database = getDatabase(myApp);
+
+export const updateDatabase = () => {
+    const token = useStore('token');
+    const notes = useStore('notes');
+
+    set( ref(database, token + '_notes'), notes );
+}
+
+export const getNotes = () => {
+    const getRef = ref(database, token + '_notes');
+
+    onValue(getRef, (snapshot) => {
+        snapshot.val() !== null ? store.dispatch('notes', snapshot.val())
+        : store.dispatch('notes', {});
+    }, { onlyOnce: true });
+}
 
 export default myApp;
