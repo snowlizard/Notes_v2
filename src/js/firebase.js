b22f47f49cc13466aeab748019513e54ce03897f
snowlizard <illegal_burrito@live.com>
2023-01-09 13:53:46 -0800

    hide db

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "portfolio-3e5da.firebaseapp.com",
-    databaseURL: "https://portfolio-3e5da-default-rtdb.firebaseio.com",
-    projectId: "portfolio-3e5da",
-    storageBucket: "portfolio-3e5da.appspot.com",
-    messagingSenderId: "362249215551",
-    appId: "1:362249215551:web:02a5e7390280562f20628a"
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIN,
+    databaseURL: import.meta.env.DATABASE_URL,
+    projectId: import.meta.env.PROJECT_ID,
+    storageBucket: import.meta.env.STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MSG_SENDER_ID,
+    appId: import.meta.env.APP_ID
 };
 
 const myApp    = initializeApp(firebaseConfig);
