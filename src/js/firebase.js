c93d61aec8d8c4d660ab699adf23d2fae12b345f
snowlizard <illegal_burrito@live.com>
2023-01-09 14:08:43 -0800

    env clientside fix

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: import.meta.env.AUTH_DOMAIN,
-    databaseURL: import.meta.env.DATABASE_URL,
-    projectId: import.meta.env.PROJECT_ID,
-    storageBucket: import.meta.env.STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.MSG_SENDER_ID,
-    appId: import.meta.env.APP_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 const myApp    = initializeApp(firebaseConfig);
